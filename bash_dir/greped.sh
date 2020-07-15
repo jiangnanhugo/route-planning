04f8a34dfb28fb4ca50c35402d73735e025b2850
nan jiang <najang@amazon.com>
2020-07-15 01:07:07 -0400

    list result

--- bash_dir/greped.sh
+++ bash_dir/greped.sh
@@ -1,10 +1,10 @@
 for loc in $(seq 2 1 29)
 do
 	echo $loc
-	echo "w crisp"
-	cat dataset/"$loc"locations/*mask1.out|grep "valid: gen="|sort|tail -n 1
-	echo "w/o crisp"
-	cat dataset/"$loc"locations/*mask0.out|grep "valid: gen="|sort|tail -n 1
+	echo "with crisp"
+	cat dataset/"$loc"locations/*crisp1.out|grep "valid routes="|sort|tail -n 1
+	echo "without crisp"
+	cat dataset/"$loc"locations/*crisp0.out|grep "valid routes=="|sort|tail -n 1
 done
 
 
