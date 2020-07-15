43d30b35d585c66b29ff12c01313e6fe08092c59
jiangnan <jiangnan.hugo@gmail.com>
2020-07-15 02:41:02 -0400

    update bash

--- bash_dir/greped.sh
+++ bash_dir/greped.sh
@@ -1,9 +1,9 @@
 for loc in $(seq 2 1 29)
 do
 	echo $loc
-	echo "with crisp"
+	echo "with crisp\c"
 	cat dataset/"$loc"locations/*crisp1.out|grep "valid routes="|sort|tail -n 1
-	echo "without crisp"
+	echo "without crisp\c"
 	cat dataset/"$loc"locations/*crisp0.out|grep "valid routes=="|sort|tail -n 1
 done
 
