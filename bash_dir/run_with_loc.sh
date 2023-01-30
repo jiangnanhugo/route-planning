c450e0716c535e9b366c0442f50c01e6667dc41b
Nan Jiang <jiang631@purdue.edu>
2023-01-30 01:37:14 -0500

    change one bug in run_with_loc.sh  Changes to be committed: 	modified:   bash_dir/run_with_loc.sh 	modified:   run.sh

--- bash_dir/run_with_loc.sh
+++ bash_dir/run_with_loc.sh
@@ -7,7 +7,7 @@ cuda=$5
 folder=dataset/${loc}locations
 
 max_width=$6
-batchnum=100
+batchnum=2
 batchnum_summary=10
 
 d_n_hidden=100
@@ -26,6 +26,6 @@ prob_file=$folder/bays29_s0_e0_md1000_ms${loc}_random_reward.prob
 echo "$prob_file use_crisp=$use_crisp post_proces_type1=$post_proces_type1, post_proces_type2=$post_proces_type2, maxwidth=$max_width"
 python3 gan_with_crisp.py --data_file $data_file --prob_file $prob_file --max_width $max_width --batch_size $batchnum \
 --test_size $batchnum_summary  --d_n_hidden $d_n_hidden --g_n_hidden $g_n_hidden --g_n_input $g_n_input \
---lr $lr --glr $glr --cuda $cuda --use_crisp $use_crisp --use_post_process_type2 $post_proces_type1 \
+--lr $lr --glr $glr --cuda $cuda --use_crisp $use_crisp --use_post_process_type1 $post_proces_type1 \
 --use_post_process_type2 $post_proces_type2 > $data_file.crisp${use_crisp}.width${max_width}.post_proces_type${post_proces_type1}.post_proces_type${post_proces_type2}.out
 
--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
-for loc in $(seq 15 1 29)
+set -x
+for width in 16 32
 do
-	./bash_dir/run_with_loc.sh $loc 1 0
+	n_loc=5
+	use_crisp=True
+	post_proces_type1=False
+	post_proces_type2=False
+	cuda_device=0
+    ./bash_dir/run_with_loc.sh $n_loc $use_crisp $post_proces_type1 $post_proces_type2 $cuda_device $width
 done
+
